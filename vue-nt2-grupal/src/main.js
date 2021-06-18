61958d91da41e6bf1df935b6340ee5c448aeb13f
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-06-17 21:21:08 -0300

    add dependencies

--- vue-nt2-grupal/src/main.js
+++ vue-nt2-grupal/src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./form";
+
 Vue.config.productionTip = false
 
 new Vue({
