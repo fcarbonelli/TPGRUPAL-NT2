e6dabba0704d6182cba5125e2967e51f234a17fc
Francisco Carbonelli <carbonelli.francisco@gmail.com>
2021-06-19 02:46:47 -0300

    agregado formulario vehiculo

--- vue-nt2-grupal/src/router/index.js
+++ vue-nt2-grupal/src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import FormularioVehiculo from '/src/components/FormularioVehiculo/index.vue';
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-  }
+  { path: '/',  name: 'Home'},
+  { path: "/vehicles", component: FormularioVehiculo}
 ]
 
 const router = new VueRouter({
