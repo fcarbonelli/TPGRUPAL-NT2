0705c2c390f16399699a69bd9f86c0508fbc0866
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-06-19 19:35:01 -0300

    add methods in store for api calls

--- vue-nt2-grupal/src/store/userModule.js
+++ vue-nt2-grupal/src/store/userModule.js
@@ -1,13 +1,40 @@
+import axios from 'axios';
+
+const URL = '';
+
 export default userModule = {
 
   state: {
-
+    users: [],
   },
-  actions: {
-
-  },
-  mutations: {
-
-  }
   
+  actions: {
+    async getUsers({commit}) {
+      try {
+        let { data } = await axios(URL);
+        commit('getUsers', data);
+      } catch(e) {
+        console.log(e);
+      }
+      return this.state.users;
+    },
+    async saveUser({commit}, userData) {
+      try {
+        let { data } = await axios.post(URL, userData);
+        commit('postUser', data);
+      } catch(e) {
+        console.log(e);
+      }
+    },
+  },
+
+  mutations: {
+    getUsers(state, data) {
+      state.users = data;
+    },
+    postUser(state, userData) {
+      state.users = [...state.users, userData];
+    },
+  }
+
 }
--- vue-nt2-grupal/src/store/vehicleModule.js
+++ vue-nt2-grupal/src/store/vehicleModule.js
@@ -1,13 +1,40 @@
+import axios from 'axios';
+
+const URL = '';
+
 export default vehicleModule = {
 
   state: {
-
+    vehicles: [],
   },
-  actions: {
-
-  },
-  mutations: {
-
-  }
   
+  actions: {
+    async getVehicles({commit}) {
+      try {
+        let { data } = await axios(URL);
+        commit('getVehicles', data);
+      } catch(e) {
+        console.log(e);
+      }
+      return this.state.vehicles;
+    },
+    async saveVehicle({commit}, vehicleData) {
+      try {
+        let { data } = await axios.post(URL, vehicleData);
+        commit('postVehicle', data);
+      } catch(e) {
+        console.log(e);
+      }
+    },
+  },
+
+  mutations: {
+    getVehicles(state, data) {
+      state.vehicles = data;
+    },
+    postVehicle(state, vehicleData) {
+      state.vehicles = [...state.vehicles, vehicleData];
+    },
+  }
+
 }
