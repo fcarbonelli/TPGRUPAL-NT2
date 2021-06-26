f67c43a55125a557ba0646260b0ee49363bb8bf8
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-06-26 12:48:30 -0300

    add vehicle store implementation

--- client/src/store/vehicleModule.js
+++ client/src/store/vehicleModule.js
@@ -18,10 +18,22 @@ const vehicleModule = {
       }
       return this.state.vehicles;
     },
-    async saveVehicle({commit}, vehicleData) {
+    async saveVehicle({commit}, vehicleData, userToken) {
       try {
-        let { data } = await axios.post(URL, vehicleData);
+        let { data } = await axios.post(URL, vehicleData, {
+          headers: {
+              'Authorization': 'Bearer ' + userToken
+          }
+        });
         commit('postVehicle', data);
+      } catch(e) {
+        console.log(e);
+      }
+    },
+    async getVehicleById(id) {
+      try {
+        let { data } = await axios.get(URL + '/' + id);
+        return data;
       } catch(e) {
         console.log(e);
       }
