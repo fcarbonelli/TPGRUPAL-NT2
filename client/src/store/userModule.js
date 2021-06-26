b1809ff235e300cf2b9cf7457f4a79192f6162a8
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-06-26 11:52:57 -0300

    add user store implementation

--- client/src/store/userModule.js
+++ client/src/store/userModule.js
@@ -6,35 +6,80 @@ const userModule = {
 
   state: {
     users: [],
+    user: {},
+    isAuth: false
   },
   
   actions: {
-    async getUsers({commit}) {
+    async registerHandler({commit}, credentials) {
       try {
-        let { data } = await axios(URL);
-        commit('getUsers', data);
-      } catch(e) {
-        console.log(e);
+          const { data } = await axios.post(URL, credentials);
+
+          localStorage.setItem('token', data.token);
+
+          if(user) {
+            commit('setIsAuthenticated', true);
+            commit('saveUser', data);
+            return true;
+          }
+
+          return false;
+      } catch (error) {
+          console.log(error);
       }
-      return this.state.users;
     },
-    async saveUser({commit}, userData) {
+    async loginHandler({commit}, credentials) {
       try {
-        let { data } = await axios.post(URL, userData);
-        commit('postUser', data);
-      } catch(e) {
-        console.log(e);
+          const { data } = await axios.post(URL + '/login', credentials);
+          localStorage.setItem('token', data.token);
+
+          commit('setIsAuthenticated', true);
+          commit('saveUser', data);
+
+          return true;
+      } catch (error) {
+          console.log(error);
       }
     },
+    async logoutHandler({commit}) {
+      const token = localStorage.getItem('token');
+      try {
+          await axios.post(URL + '/logout', {} , {
+                  headers: {
+                      'Authorization': 'Bearer ' + token
+                  }
+          });
+
+          localStorage.removeItem('token');
+
+          commit('setIsAuthenticated', true);
+          commit('saveUser', {});
+
+      } catch (error) {
+          console.log(error);
+      }
+  }
+    // async getUsers({commit}) {
+    //   try {
+    //     let { data } = await axios(URL);
+    //     commit('getUsers', data);
+    //   } catch(e) {
+    //     console.log(e);
+    //   }
+    //   return this.state.users;
+    // },
   },
 
   mutations: {
     getUsers(state, data) {
       state.users = data;
     },
-    postUser(state, userData) {
+    saveUser(state, userData) {
       state.users = [...state.users, userData];
     },
+    setIsAuthenticated(state, isAuth) {
+      state.isAuth = isAuth;
+    }
   }
 
 }
